a5a82472b3470569b62f899309d5c47ab47ebbba
monjednidal <monjed1010@gamil.com>
2022-09-11 13:44:38 +0300

    commit

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }))
-app.get('/cars', (req, res) => {
-    res.send('get request done!')
+app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Monjed',
+        comment: 'lololo hahahahah'
+    },
+    {
+        username: 'Mohammad',
+        comment: 'lololo nice one'
+    },
+    {
+        username: 'Yousef',
+        comment: 'ok that was goood!'
+    },
+    {
+        username: 'ali',
+        comment: 'Thank u man'
+    },
+    {
+        username: 'Sami',
+        comment: 'Happy birthday!!'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
 })
-app.post('/cars', (req, res) => {
-    console.log(req.body);
-    res.send('post request done!')
-})
+
+// app.get('/cars', (req, res) => {
+//     res.send('get request done!')
+// })
+// app.post('/cars', (req, res) => {
+//     console.log(req.body);
+//     res.send('post request done!')
+// })
 
 
 
