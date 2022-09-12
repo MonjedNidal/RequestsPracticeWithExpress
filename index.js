22e87a2c329f8348ad7b8e74204b019bc8340108
monjednidal <monjed1010@gamil.com>
2022-09-13 01:40:22 +0300

    Simple form to edit comment added

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -55,13 +58,27 @@ app.post('/comments', (req, res) => {
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id == id);
+    if (comment == undefined) {
+        res.render('comments/index', { comments });
+    }
     res.render('comments/show', { comment });
 })
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    if (comment == undefined) {
+        res.render('comments/index', { comments });
+    }
+    res.render('comments/edit', { comment });
+})
+
 app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const newComment = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
     foundComment.comment = newComment;
+    res.redirect('comments');
 })
 app.listen(3000, () => {
     console.log('Listening to port 3000');
